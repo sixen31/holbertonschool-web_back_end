327d7740814090c9ea329429242cc00eb39eebcf
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-28 02:14:58 -0700

    Define and annotate the following variables with the specified values

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module contains a function to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to its string representation.
+    """
+    return str(n)
---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines variables with specific values.
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
+
