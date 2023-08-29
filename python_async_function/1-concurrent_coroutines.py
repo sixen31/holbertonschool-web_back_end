5ddf400236a0a372f57e694b7f27e5df39dbcb49
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-29 02:50:24 -0700

    Import wait_random from the previous python file that you’ve written and write an async routine called

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This asynchronous routine that spawns multiple wait_random coroutines.
+"""
+
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns multiple wait_random coroutines and returns a list of their delays
+    """
+    delays = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delays)
