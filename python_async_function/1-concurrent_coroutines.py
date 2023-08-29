76d0b51e63e58402b3d1faf650a72b1a03044668
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-29 02:54:19 -0700

    change indentation add new line at line 13

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,6 +10,7 @@ from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Spawns multiple wait_random coroutines and returns a list of their delays
