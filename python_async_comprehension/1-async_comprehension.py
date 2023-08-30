181a0e27e3dc876ed505cd3b0023fd6516794e93
sixen31 <alexisgrandjean@hotmail.fr>
2023-08-30 00:41:09 -0700

    add comment and sup over line 80

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-
+async comprehension
 """
 
 import asyncio
@@ -10,6 +10,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Coroutine that collects 10 random numbers using an async comprehension over async_generator
+10 random numbers using an async comprehension over async_generator
     """
     return [i async for i in async_generator()][:10]
