14f1ccf880794a0d79b9369e2c9bc30d8821f12f
alex <6267@holbertonstudents.com>
2024-01-02 01:59:36 -0800

    reprise code

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -15,5 +15,4 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     Spawns multiple wait_random coroutines and returns a list of their delays
     """
     delays = [await wait_random(max_delay) for _ in range(n)]
-    results = await asyncio.gather(*tasks)
     return sorted(delays)
