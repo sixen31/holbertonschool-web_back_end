9a6638d4f279706ffc1b2106c2bbb0df5ae496c0
alex <6267@holbertonstudents.com>
2024-01-26 03:47:52 -0800

    correct

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,11 +2,11 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [picture, user] = results.map(result => result.value);
-      console.log(`${picture.body} ${user.firstName} ${user.lastName}`);
-    })
-    .catch(() => {
+    .then((values) => {
+      const photo = values[0];
+      const user = values[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    }).catch(() => {
       console.log('Signup system offline');
     });
 }
