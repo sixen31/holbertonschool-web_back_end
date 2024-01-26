16a47bd37aab32713be9413b7c2ad88f30810aaf
alex <6267@holbertonstudents.com>
2024-01-26 03:41:25 -0800

    In this file, import uploadPhoto and createUser from utils.js Knowing that the functions in utils.js return promises, use the prototype below to collectively resolve all promises and log body firstName lastName to the console.function handleProfileSignup() In the event of an error, log Signup system offline to the console

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  Promise.allSettled([uploadPhoto(), createUser()])
+    .then((results) => {
+      const successes = results.filter(result => result.status === 'fulfilled');
+      
+      if (successes.length === 2) {
+        const [picture, user] = successes.map(result => result.value);
+        console.log(`${picture.body} ${user.firstName} ${user.lastName}`);
+      } else {
+        console.log('Signup system offline');
+      }
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
