a065da4a7bc1721de64e93426f64bce4da9ac595
alex <6267@holbertonstudents.com>
2024-01-26 03:45:25 -0800

    Using Promise.all to wait for the resolution of all promises

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,16 +1,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.allSettled([uploadPhoto(), createUser()])
+  Promise.all([uploadPhoto(), createUser()])
     .then((results) => {
-      const successes = results.filter(result => result.status == 'fulfilled');
-      
-      if (successes.length == 2) {
-        const [picture, user] = successes.map(result => result.value);
-        console.log(`${picture.body} ${user.firstName} ${user.lastName}`);
-      } else {
-        console.log('Signup system offline');
-      }
+      const [picture, user] = results.map(result => result.value);
+      console.log(`${picture.body} ${user.firstName} ${user.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
